9a66074b54a2778fe925d2f3dba2f9cfcb04ec5c
abatar1 <evgenyhalzov@gmail.com>
2024-02-08 17:35:05 +0200

    more tests added

---
+++ SimpleFluentTester.UnitTests/TestCaseTests.cs
@@ -0,0 +1,38 @@
+﻿using SimpleFluentTester.Entities;
+
+namespace SimpleFluentTester.UnitTests;
+
+public class TestCaseTests
+{
+    [Fact]
+    public void TestCase_CreateMultipleInputsAndToString_ShouldNotThrow()
+    {
+        // Assign
+        var lazyResult = new Lazy<CalculatedTestResult<int>>(new CalculatedTestResult<int>(true, new ValueWrapper<int>(2),
+                new Exception(), TimeSpan.Zero));
+        _ = lazyResult.Value;
+        var testCase = new TestCase<int>(new[] { 1, 2 }.Cast<object>().ToArray(), 3, lazyResult, true, 1);
+
+        // Act
+        var printedResult = testCase.ToString();
+
+        // Assert
+        Assert.NotEmpty(printedResult);
+    }
+    
+    [Fact]
+    public void TestCase_CreateSingleInputAndToString_ShouldNotThrow()
+    {
+        // Assign
+        var lazyResult = new Lazy<CalculatedTestResult<int>>(new CalculatedTestResult<int>(true, new ValueWrapper<int>(2),
+            new Exception(), TimeSpan.Zero));
+        _ = lazyResult.Value;
+        var testCase = new TestCase<int>(new[] { 1 }.Cast<object>().ToArray(), 3, lazyResult, true, 1);
+
+        // Act
+        var printedResult = testCase.ToString();
+
+        // Assert
+        Assert.NotEmpty(printedResult);
+    }
+}
